93c3e772f38093489fda2bce56e6a00b968543d4
Dustin Peukert <dpeukert04@gmail.com>
2024-10-18 21:21:17 -0400

    add attributes; write test for #set_registration_date

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'date'
 
 RSpec.describe Vehicle do
   before(:each) do
@@ -15,7 +16,8 @@ RSpec.describe Vehicle do
       expect(@cruz.make).to eq('Chevrolet')
       expect(@cruz.model).to eq('Cruz')
       expect(@cruz.engine).to eq(:ice)
-      expect(@cruz.registration_date).to eq(nil)
+      expect(@cruz.registration_date).to be nil
+      expect(@cruz.plate_type).to be nil
     end
   end
 
@@ -32,6 +34,17 @@ RSpec.describe Vehicle do
       expect(@cruz.electric_vehicle?).to eq(false)
       expect(@bolt.electric_vehicle?).to eq(true)
       expect(@camaro.electric_vehicle?).to eq(false)
+    end
+  end
+
+  describe '#set_registration_date' do
+    it 'sets the date of registration upon method call' do
+      expect(@cruz.registration_date).to be nil
+
+      @cruz.set_registration_date
+
+      expect(@registration_date).to be_an_instance_of(Date)
+      expect(@registration_date).to eq(Date.today)
     end
   end
 end
