c226a20a9c2dca47f506e8adcfbb308a3b80f7e9
Dustin Peukert <dpeukert04@gmail.com>
2024-10-18 22:41:38 -0400

    write tests for #register_vehicle

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -32,4 +32,41 @@ RSpec.describe Facility do
       expect(@facility_1.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
     end
   end
+
+  describe '#register_vehicle' do
+    it 'can register a vehicle' do
+      expect(@cruz.registration_date).to be nil
+      expect(@cruz.plate_type).to be nil
+      expect(@facility_1.registered_vehicles).to eq([])
+      
+      expect(@facility_1.register_vehicle(@cruz)).to eq([@cruz])
+
+      expect(@cruz.registration_date).to be_a(Date) #Date.today
+      expect(@cruz.registration_date).to eq(Date.today)
+      expect(@cruz.plate_type).to eq(:regular)
+
+      expect(@facility_1.registered_vehicles).to eq([@cruz])
+    end
+
+    it 'can only do so if facility provides that service' do
+      expect(@facility_1.services).to eq(['Vehicle Registration'])
+      expect(@facility_2.services).to eq([])
+
+      expect(@facility_1.register(@cruz)).to eq([@cruz])
+      expect(@facility_2.register(@camaro)).to eq('We do not provide this service: Vehicle Registration')
+
+      expect(@facility_1.registered_vehicles).to eq([@cruz])
+      expect(@facility_2.registered_vehicles).to eq([])
+    end
+
+    it 'can register more than 1 vehicle' do
+      expect(@facility_1.services).to eq(['Vehicle Registration'])
+
+      expect(@facility_1.register(@cruz)).to eq([@cruz])
+      expect(@facility_1.register(@camaro)).to eq([@cruz, @camaro])
+      expect(@facility_1.register(@bolt)).to eq([@cruz, @camaro, @bolt])
+
+      expect(@facility_1.services).to eq([@cruz, @camaro, @bolt])
+    end
+  end
 end
