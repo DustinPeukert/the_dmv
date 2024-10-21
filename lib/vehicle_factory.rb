212a01bccc73b2b628ec199a93d2460bf5e0d511
Dustin Peukert <dpeukert04@gmail.com>
2024-10-21 13:36:34 -0400

    format syntax

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -4,11 +4,11 @@ class VehicleFactory
 
     vehicle_data.each do |vehicle|
       new_vehicle = Vehicle.new({vin: vehicle[:"vin_1_10"],
-                             year: vehicle[:"model_year"].to_i,
-                             make: vehicle[:"make"].capitalize,
-                             model: vehicle[:"model"],
-                             engine: :ev
-                            })
+                                 year: vehicle[:"model_year"].to_i,
+                                 make: vehicle[:"make"].capitalize,
+                                 model: vehicle[:"model"],
+                                 engine: :ev
+                                })
 
       created_vehicles << new_vehicle
     end
